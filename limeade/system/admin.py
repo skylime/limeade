a0313f8a6a08519c456630c24ac0b93b9e8ae1ee
Thomas Merkel <tm@core.io>
2012-03-03 15:42:45 +0100

    add admin interface product limits

--- limeade/system/admin.py
+++ limeade/system/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from models import Person, Product, Contract, Domain
+from limeade.web.models import Limitset as WebLimitset
 from limeade.mail.models import Limitset as MailLimitset
+from limeade.cloud.models import Limitset as CloudLimitset
+from limeade.mysql.models import Limitset as MySQLLimitset
+from limeade.ftp.models import Limitset as FTPLimitset
 
 class PersonAdmin(admin.ModelAdmin):
 	list_display = ('first_name', 'last_name', 'company', 'address', 'parent')
@@ -9,32 +13,47 @@ class PersonAdmin(admin.ModelAdmin):
 	list_filter = ['parent']
 	
 
-class MailLimitsetInline(admin.StackedInline):
-        model = MailLimitset
+class WebLimitsetInline(admin.StackedInline):
+	model = WebLimitset
 
+class MailLimitsetInline(admin.StackedInline):
+	model = MailLimitset
+
+class CloudLimitsetInline(admin.StackedInline):
+	model = CloudLimitset
+
+class MySQLLimitsetInline(admin.StackedInline):
+	model = MySQLLimitset
+
+class FTPLimitsetInline(admin.StackedInline):
+	model = FTPLimitset
 
 class ProductAdmin(admin.ModelAdmin):
-        inlines = [
-                MailLimitsetInline,
-                ]
+	inlines = [
+		WebLimitsetInline,
+		MailLimitsetInline,
+		CloudLimitsetInline,
+		MySQLLimitsetInline,
+		FTPLimitsetInline,
+	]
 
 class DomainInline(admin.TabularInline):
-        model = Domain
-        extra = 3
+	model = Domain
+	extra = 3
 
 class DomainAdmin(admin.ModelAdmin):
-        list_display = ('name', 'owner', 'contract')
-        search_fields = ['name']
+	list_display = ('name', 'owner', 'contract')
+	search_fields = ['name']
 
 class ContractAdmin(admin.ModelAdmin):
-        inlines = [DomainInline]
+	inlines = [DomainInline]
 
 
 class PersonAdmin(admin.ModelAdmin):
-        list_display = ('first_name', 'last_name', 'company', 'address', 'parent')
-        list_display_links = ('first_name', 'last_name', 'company')
-        search_fields = ['company']
-        list_filter = ['parent']
+	list_display = ('first_name', 'last_name', 'company', 'address', 'parent')
+	list_display_links = ('first_name', 'last_name', 'company')
+	search_fields = ['company']
+	list_filter = ['parent']
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Product, ProductAdmin)
