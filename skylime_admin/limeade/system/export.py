f9fe5d4b893d0ba8c7c3be2eceef44345ba8df5f
Thomas Merkel <tm@core.io>
2012-04-18 15:02:07 +0000

    change export for system users, use a variable

--- skylime_admin/limeade/system/export.py
+++ skylime_admin/limeade/system/export.py
@@ -6,12 +6,16 @@ from models import Person
 def user_export(request):
 	response = HttpResponse(mimetype='text/plain')
 	response.write(export_header())
+	user_tpl  = "$system_user = {\n"
+	group_tpl = "$system_group = {\n"
 	for p in Person.objects.all():
 		username = p.system_user_name()
 		home     = p.system_user_home()
 		uid      = p.system_user_id()
-		user_tpl  = "user  { '%s': ensure => present, uid => %s, gid => %s, shell => '/bin/false', home => '%s', managehome => true, require => Group['%s'] }\n"
-		group_tpl = "group { '%s': ensure => present, gid => %s }\n"
-		response.write(user_tpl  % (username, uid, uid, home, username))
-		response.write(group_tpl % (username, uid))
+		user_tpl  += "\t'%s' => { ensure => present, uid => %s, gid => %s, shell => '/bin/false', home => '%s', managehome => true, require => Group['%s'] }\n"
+		group_tpl += "\t'%s' => { ensure => present, gid => %s }\n"
+	user_tpl  += "}\n"
+	group_tpl += "}\n"
+	response.write(user_tpl  % (username, uid, uid, home, username))
+	response.write(group_tpl % (username, uid))
 	return response
