221a90ee25515347d57ea07358c03ea6345f6414
Marc Rochow <marc.rochow@gironimo.org>
2012-05-21 10:37:23 +0200

    removed duplicate code

--- web/limeade/system/admin.py
+++ web/limeade/system/admin.py
@@ -1,61 +1,70 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
-from models import Person, Product, Contract, Domain
-from limeade.web.models import Limitset as WebLimitset
-from limeade.mail.models import Limitset as MailLimitset
-from limeade.cloud.models import Limitset as CloudLimitset
-from limeade.mysql.models import Limitset as MySQLLimitset
-from limeade.ftp.models import Limitset as FTPLimitset
+
+from ..cloud.models import Limitset as CloudLimitset
+from ..mysql.models import Limitset as MySQLLimitset
+from ..mail.models import Limitset as MailLimitset
+from ..web.models import Limitset as WebLimitset
+from ..ftp.models import Limitset as FTPLimitset
+from .models import Person
+from .models import Product
+from .models import Contract
+from .models import Domain
+
 
 class PersonAdmin(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'company', 'address', 'parent')
-	list_display_links = ('first_name', 'last_name', 'company')
-	search_fields = ['first_name', 'last_name', 'company']
-	list_filter = ['parent']
-	
+    list_display = ('first_name', 'last_name', 'company', 'address', 'parent',)
+    list_display_links = ('first_name', 'last_name', 'company',)
+    search_fields = ['first_name', 'last_name', 'company',]
+    list_filter = ['parent',]
+
 
 class WebLimitsetInline(admin.StackedInline):
-	model = WebLimitset
+    model = WebLimitset
+
 
 class MailLimitsetInline(admin.StackedInline):
-	model = MailLimitset
+    model = MailLimitset
+
 
 class CloudLimitsetInline(admin.StackedInline):
-	model = CloudLimitset
+    model = CloudLimitset
+
 
 class MySQLLimitsetInline(admin.StackedInline):
-	model = MySQLLimitset
+    model = MySQLLimitset
+
 
 class FTPLimitsetInline(admin.StackedInline):
-	model = FTPLimitset
+    model = FTPLimitset
+
 
 class ProductAdmin(admin.ModelAdmin):
-	inlines = [
-		WebLimitsetInline,
-		MailLimitsetInline,
-		CloudLimitsetInline,
-		MySQLLimitsetInline,
-		FTPLimitsetInline,
-	]
+    inlines = [
+        WebLimitsetInline,
+        MailLimitsetInline,
+        CloudLimitsetInline,
+        MySQLLimitsetInline,
+        FTPLimitsetInline,
+    ]
+
 
 class DomainInline(admin.TabularInline):
-	model = Domain
-	extra = 3
+    model = Domain
+    extra = 3
+
 
 class DomainAdmin(admin.ModelAdmin):
-	list_display = ('name', 'owner', 'contract')
-	search_fields = ['name']
+    list_display = ('name', 'owner', 'contract',)
+    search_fields = ['name',]
+
 
 class ContractAdmin(admin.ModelAdmin):
-	inlines = [DomainInline]
+    inlines = [DomainInline,]
 
-
-class PersonAdmin(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'company', 'address', 'parent')
-	list_display_links = ('first_name', 'last_name', 'company')
-	search_fields = ['company']
-	list_filter = ['parent']
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Contract, ContractAdmin)
 admin.site.register(Domain, DomainAdmin)
+
