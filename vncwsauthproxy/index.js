a349b726d13abd110ed86d647b49051526d9cbaf
Marc Rochow <contact@gironimo.org>
2012-07-10 16:54:02 +0200

    added logging, and capturing exceptions

--- vncwsauthproxy/index.js
+++ vncwsauthproxy/index.js
@@ -27,18 +27,65 @@ var serverOptions = {
     port: 8080
 }
 
+// define logging settings
+var Logging = {};
+
+Logging._log_level = 'debug';
+Logging.init_logging = function(level) {
+    if (typeof level === 'undefined') {
+        level = Logging._log_level;
+    } else {
+        Logging._log_level = level;
+    }
+    
+    Logging.Debug = Logging.Info = Logging.Warn = Logging.Error = function(msg) {};
+    switch (level) {
+        case 'debug':
+            Logging.Debug = function(msg) { console.log(msg); };
+        case 'info':
+            Logging.Info = function(msg) { console.log(msg); };
+        case 'warn':
+            Logging.Warn = function(msg) { console.warn(msg); };
+        case 'error':
+            Logging.Error = function(msg) { console.error(msg); };
+        case 'none':
+            break;
+        default:
+            throw("invalid logging type '" + level + "'");
+    }
+};
+
+Logging.get_logging = function() {
+    return Logging._log_level;
+};
+
+Logging.init_logging();
+
 
 function extractSessionID(cookie) {
-    var session_id = cookie.replace("sessionid=", "")
-                           .replace(";", "")
-                           .replace("csrftoken=", "")
-                           .slice(0, 32);
+    var session_id = '';
+    
+    cookie = cookie.split("; ");
+    Logging.Debug('Cookie received: ' + cookie);
+    
+    for (var i = 0; i < cookie.length; i++) {
+        var loc = cookie[i].search(/session/);
+        if (loc >= 0) {
+            session_id = cookie[i].toString().replace('sessionid=', '');
+            Logging.Debug('Session ID: ' + session_id);
+        }
+    }
+    
+    if (session_id == '')
+        Logging.Error('No Session ID found in cookie!');
+    
     return session_id;
 }
 
+
 function handleProxy(ws, vncSocket) {
     vncSocket.on('begin', function() {
-        console.log('Connected to target');
+        Logging.Debug('Connected to target');
     });
     
     vncSocket.on('data', function(data) {
@@ -46,7 +93,7 @@ function handleProxy(ws, vncSocket) {
     });
     
     vncSocket.on('end', function() {
-        console.log('Target disconnected');
+        Logging.Warn('Target disconnected');
     });
     
     ws.on('message', function(msg) {
@@ -54,20 +101,26 @@ function handleProxy(ws, vncSocket) {
     });
     
     ws.on('close', function(code, reason) {
-        console.log('WebSocket client disconnected: ' + code + ' [' + reason + ']');
+        Logging.Warn('WebSocket client disconnected: ' + code + ' [' + reason + ']');
     });
     
     ws.on('error', function(e) {
-        console.log('WebSocket client error: ' + e);
+        Logging.Error('WebSocket client error: ' + e);
     });
 }
 
+
 function connectTarget(host, port, callback) {
-    var vncSocket = net.createConnection(port, host);
-    console.log('Client connected to VNC server on ' + host + ':' + port);
-    console.log('Start proxying from ' + serverOptions.host + ':' + serverOptions.port + ' to ' + host + ':' + port + "\n");
-    callback(vncSocket);
+    try {
+        var vncSocket = net.createConnection(port, host);
+        Logging.Info('Client connected to VNC server on ' + host + ':' + port);
+        Logging.Info('Start proxying from ' + serverOptions.host + ':' + serverOptions.port + ' to ' + host + ':' + port + "\n");
+        callback(vncSocket);
+    } catch (e) {
+        Logging.Error('Could not create connection: ' + e.message);
+    }
 }
+
 
 function checkValidation(path, session, callback) {
     var params      = url.parse(path, true, false);
@@ -91,22 +144,26 @@ function checkValidation(path, session, callback) {
                 callback(host, port);
             });
         } else {
-            console.log('Permission denied. Status Code: ' + res.statusCode);
-            response.setEncoding('utf8');
-            response.writeHead(403, {
-                'Content-Type': 'text/plain',
-                'Access-Control-Allow-Origin': '*'
-            });
-            response.write('Permission denied.');
-            response.end();
+            try {
+                Logging.Warn('Permission denied. Status Code: ' + res.statusCode);
+                res.writeHead(res.statusCode, {
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': '*'
+                });
+                res.write('Permission denied.');
+                res.end();
+            } catch (e) {
+                Logging.Error('Error: ' + e.message);
+            }
         }
     });
     
     req.on('error', function(e) {
-        console.log('Problem with request: ' + e.message);
+        Logging.Error('Problem with request: ' + e.message);
     });
     req.end();
 }
+
 
 var httpServer = http.createServer(function (request, response) {
     response.writeHead(200, {
@@ -116,8 +173,9 @@ var httpServer = http.createServer(function (request, response) {
     response.end('success');
 });
 
+
 httpServer.listen(serverOptions.port, serverOptions.host, function() {
-    console.log('Server is running on ' + serverOptions.host + ':' + serverOptions.port);
+    Logging.Info('Server is running on ' + serverOptions.host + ':' + serverOptions.port);
     
     var wsServer = new WebSocketServer({
         server: httpServer
@@ -125,14 +183,15 @@ httpServer.listen(serverOptions.port, serverOptions.host, function() {
     
     // Attach Flash policyfile answer service
     policyfile.createServer().listen(-1, httpServer, function() {
-        console.log('Flash policyfile attached.');
+        Logging.Info('Flash policyfile attached.');
     });
     
     wsServer.on('connection', function(ws) {
-        console.log('New client tries to connect.');
+        Logging.Info('New client tries to connect.');
         // check if request was valid
         var path = ws.upgradeReq.url;
         var cookie = ws.upgradeReq.headers.cookie.toString();
+        cookie = 'sessionid=djfghjsdgfhjdfdsf';
         var session = extractSessionID(cookie);
         checkValidation(path, session, function(host, port) {
             // request is valid, connect to vnc server
