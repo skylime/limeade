84d52d779eec78631a9233407c7e1f8994a61722
Marc Rochow <contact@gironimo.org>
2012-09-09 00:09:04 +0200

    mail docstrings

--- web/limeade/mail/models.py
+++ web/limeade/mail/models.py
@@ -1,50 +1,79 @@
+"""Models for limeade mail"""
 from django.db import models
 from limeade.system.models import Product, Domain
 from limeade.system.utils import get_domains, md5crypt, gen_salt
 
+
+# define default length for Django's ORM
 default_length = 250
 
-class Account(models.Model):
-	name     = models.CharField(max_length=default_length)
-	domain   = models.ForeignKey(Domain)
-	password = models.CharField(max_length=default_length)
-	
-	def set_password(self, password):
-		self.password = md5crypt(password, gen_salt())
 
-	def __unicode__(self):
-		return unicode(self.name) + '@' + unicode(self.domain)
-	
-	class Meta:
-		unique_together = ('name', 'domain')
-	
+class Account(models.Model):
+    """Creates a new mail account
+    
+    :param name: the name of the account
+    :param domain: the foreign key to the domain
+    :param password: the password of the account
+    """
+    name     = models.CharField(max_length=default_length)
+    domain   = models.ForeignKey(Domain)
+    password = models.CharField(max_length=default_length)
+    
+    def set_password(self, password):
+        """Salt and hashes the password."""
+        self.password = md5crypt(password, gen_salt())
+
+    def __unicode__(self):
+        """Unicode representation for the mail account."""
+        return unicode(self.name) + '@' + unicode(self.domain)
+    
+    class Meta:
+        unique_together = ('name', 'domain')
+
 
 class Redirect(models.Model):
-	name   = models.CharField(max_length=default_length)
-	domain = models.ForeignKey(Domain)
-	to     = models.CharField(max_length=default_length)
-	
-	class Meta:
-		unique_together = ('name', 'domain')
-	
-	def __unicode__(self):
-		return self.name + '@' + unicode(self.domain) + ' -> ' + self.to
-		
-class Limitset(models.Model):
-	product = models.ForeignKey(Product, unique=True, related_name='limitset_mail')
-	accounts = models.IntegerField("Accounts")
-	redirects = models.IntegerField("Redirects")
-	mailspace = models.IntegerField("Storage", help_text="MB")
+    """Creates a new mail redirect
+    
+    :param name: the name of the redirect
+    :param domain: the foreign key to the domain
+    :param to: redirect to
+    """
+    name   = models.CharField(max_length=default_length)
+    domain = models.ForeignKey(Domain)
+    to     = models.CharField(max_length=default_length)
+    
+    class Meta:
+        unique_together = ('name', 'domain')
+    
+    def __unicode__(self):
+        """Unicode representation for the mail redirect."""
+        return self.name + '@' + unicode(self.domain) + ' -> ' + self.to
 
-	is_limitset = True
-	@staticmethod
-	def utilization(user, ressource):
-		domains = get_domains(user)
-		if ressource == 'accounts':
-			return Account.objects.filter(domain__in=list(domains)).count()
-		if ressource == 'redirects':
-			return Redirect.objects.filter(domain__in=list(domains)).count()
-		return None
-		
-	class Meta:
-		verbose_name = 'Email'
+
+class Limitset(models.Model):
+    """Creates a limitset.
+    
+    :param product: the foreign key to the product
+    :param accounts: the maximum number of mail accounts
+    :param redirects: the maximum number of mail redirects
+    :param mailspace: the maximum number of space
+    """
+    product = models.ForeignKey(Product, unique=True, related_name='limitset_mail')
+    accounts = models.IntegerField("Accounts")
+    redirects = models.IntegerField("Redirects")
+    mailspace = models.IntegerField("Storage", help_text="MB")
+    
+    is_limitset = True
+    @staticmethod
+    def utilization(user, ressource):
+        """Returns the correct ressource."""
+        domains = get_domains(user)
+        if ressource == 'accounts':
+            return Account.objects.filter(domain__in=list(domains)).count()
+        if ressource == 'redirects':
+            return Redirect.objects.filter(domain__in=list(domains)).count()
+        return None
+    
+    class Meta:
+        verbose_name = 'Email'
+
--- web/limeade/mail/views.py
+++ web/limeade/mail/views.py
@@ -1,3 +1,4 @@
+"""Views for limeade mail"""
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.views.generic.list_detail import object_list, object_detail
@@ -6,80 +7,135 @@ from limeade.system.utils import get_domains
 from models import Account, Redirect
 from forms import AccountForm, AccountEditForm, RedirectForm
 
-# accounts 
 
 @login_required
 def account_list(request):
-	domains = get_domains(request.user)
-	return object_list(request, Account.objects.filter(domain__in = domains), template_name='limeade_mail/account_list.html')
+    """List all mail accounts of a user.
+    
+    :param request: the request object
+    
+    :returns: a list of mail accounts
+    """
+    domains = get_domains(request.user)
+    return object_list(request, Account.objects.filter(domain__in = domains), template_name='limeade_mail/account_list.html')
+
 
 @login_required
 def account_add(request):
-	form = AccountForm(request.POST or None)
-	form.fields['domain'].queryset = get_domains(request.user)
-	if form.is_valid():
-		ac = form.save(commit=False)
-		ac.set_password(form.cleaned_data['password'])
-		ac.save()
-		return redirect('limeade_mail_account_list')
-	return render_to_response("limeade_mail/account_add.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Add a new mail account.
+    
+    :param request: the request object
+    
+    :returns: an edit form template
+    """
+    form = AccountForm(request.POST or None)
+    form.fields['domain'].queryset = get_domains(request.user)
+    if form.is_valid():
+        ac = form.save(commit=False)
+        ac.set_password(form.cleaned_data['password'])
+        ac.save()
+        return redirect('limeade_mail_account_list')
+    return render_to_response("limeade_mail/account_add.html",
+        {"form": form}, context_instance = RequestContext(request))
 
-@login_required	
+
+@login_required 
 def account_edit(request, slug):
-	account = Account.objects.get(pk=slug)
-	if account.domain.owner() != request.user:
-		return redirect('limeade_mail_account_list')		
-	form = AccountEditForm(request.POST or None, instance=account)
-	if form.is_valid():
-		ac = form.save(commit=False)
-		ac.set_password(form.cleaned_data['password'])
-		ac.save()
-		return redirect('limeade_mail_account_list')
-	return render_to_response("limeade_mail/account_edit.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Set a new password for an email account.
+    
+    :param request: the request object
+    :param slug: the id of the account
+    
+    :returns: an edit form template
+    """
+    account = Account.objects.get(pk=slug)
+    if account.domain.owner() != request.user:
+        return redirect('limeade_mail_account_list')        
+    form = AccountEditForm(request.POST or None, instance=account)
+    if form.is_valid():
+        ac = form.save(commit=False)
+        ac.set_password(form.cleaned_data['password'])
+        ac.save()
+        return redirect('limeade_mail_account_list')
+    return render_to_response("limeade_mail/account_edit.html",
+        {"form": form}, context_instance = RequestContext(request))
+
 
 @login_required
 def account_delete(request, slug):
-	ac = get_object_or_404(Account, pk = slug)
-	if ac.domain.owner() == request.user:
-		ac.delete()
-	return redirect('limeade_mail_account_list')
-	
-	
-# redirects
+    """Remove a mail account.
+    
+    :param request: the request object
+    :param slug: the id of the account
+    
+    :returns: redirects to mail account list
+    """
+    ac = get_object_or_404(Account, pk = slug)
+    if ac.domain.owner() == request.user:
+        ac.delete()
+    return redirect('limeade_mail_account_list')
+
+
 @login_required
 def redirect_list(request):
-	domains = get_domains(request.user)
-	return object_list(request, Redirect.objects.filter(domain__in = domains), template_name='limeade_mail/redirect_list.html')
+    """List all mail redirects.
+    
+    :param request: the request object
+    
+    :returns: a list of mail redirects
+    """
+    domains = get_domains(request.user)
+    return object_list(request, Redirect.objects.filter(domain__in = domains), template_name='limeade_mail/redirect_list.html')
+
 
 @login_required
 def redirect_add(request):
-	form = RedirectForm(request.POST or None)
-	form.fields['domain'].queryset = get_domains(request.user)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_mail_redirect_list')
-	return render_to_response("limeade_mail/redirect_add.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Create a new mail redirect.
+    
+    :param request: the request object
+    
+    :returns: an edit form template
+    """
+    form = RedirectForm(request.POST or None)
+    form.fields['domain'].queryset = get_domains(request.user)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_mail_redirect_list')
+    return render_to_response("limeade_mail/redirect_add.html",
+        {"form": form}, context_instance = RequestContext(request))
 
-@login_required	
+
+@login_required 
 def redirect_edit(request, slug):
-	r = Redirect.objects.get(pk=slug)
-	if r.domain.owner() != request.user:
-		return redirect('limeade_mail_redirect_list')		
-	form = RedirectForm(request.POST or None, instance=r)
-	form.fields['domain'].queryset = get_domains(request.user)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_mail_redirect_list')
-	return render_to_response("limeade_mail/redirect_edit.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Edit a mail redirect.
+    
+    :param request: the request object
+    :param slug: the id of the redirect
+    
+    :returns: an edit form template
+    """
+    r = Redirect.objects.get(pk=slug)
+    if r.domain.owner() != request.user:
+        return redirect('limeade_mail_redirect_list')       
+    form = RedirectForm(request.POST or None, instance=r)
+    form.fields['domain'].queryset = get_domains(request.user)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_mail_redirect_list')
+    return render_to_response("limeade_mail/redirect_edit.html",
+        {"form": form}, context_instance = RequestContext(request))
+
 
 @login_required
 def redirect_delete(request, slug):
-	r = get_object_or_404(Redirect, pk = slug)
-	if r.domain.owner() == request.user:
-		r.delete()
-	return redirect('limeade_mail_redirect_list')
-	
+    """Remove a mail redirect.
+    
+    :param request: the request object
+    :param slug: the id of the redirect
+    
+    :returns: redirects to a list of mail redirects
+    """
+    r = get_object_or_404(Redirect, pk = slug)
+    if r.domain.owner() == request.user:
+        r.delete()
+    return redirect('limeade_mail_redirect_list')
