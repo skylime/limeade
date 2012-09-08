6d3511fb08ee23259e3a6de57195a7c64e1b61a4
Marc Rochow <contact@gironimo.org>
2012-09-09 00:29:49 +0200

    cluster docstrings

--- web/limeade/cluster/models.py
+++ web/limeade/cluster/models.py
@@ -1,27 +1,50 @@
+"""Models for limeade cluster"""
 from django.db import models
+
 
 default_length = 250
 
 
 class Service(models.Model):
-	name = models.CharField(max_length=default_length, unique=True)
-	
-	def __unicode__(self):
-		return unicode(self.name)
-	
+    """Creates a cluster service.
+    
+    :param name: name of the service
+    """
+    name = models.CharField(max_length=default_length, unique=True)
+    
+    def __unicode__(self):
+        """Unicode representation for the service."""
+        return unicode(self.name)
+
+
 class Region(models.Model):
-	name = models.CharField(max_length=default_length, unique=True)
-	
-	def __unicode__(self):
-		return unicode(self.name)
+    """Creates a cluster region.
+    
+    :param name: name of the region
+    """
+    name = models.CharField(max_length=default_length, unique=True)
+    
+    def __unicode__(self):
+        """Unicode representation for the region."""
+        return unicode(self.name)
+
 
 class Server(models.Model):
-	hostname = models.CharField(max_length=default_length, unique=True)
-	ip       = models.CharField(max_length=default_length, unique=True)
-	region   = models.ForeignKey(Region)
-	services = models.ManyToManyField(Service)
-	enabled  = models.BooleanField(default=False)
+    """Creates a cluster server.
+    
+    :param hostname: name of the region
+    :param ip: the ip of the server
+    :param region: foreign key to cluster region
+    :param services: foreign key to cluster service
+    :param enabled: indicates if this server is enabled
+    """
+    hostname = models.CharField(max_length=default_length, unique=True)
+    ip       = models.CharField(max_length=default_length, unique=True)
+    region   = models.ForeignKey(Region)
+    services = models.ManyToManyField(Service)
+    enabled  = models.BooleanField(default=False)
 
-	def __unicode__(self):
-		return unicode(self.hostname)
+    def __unicode__(self):
+        """Unicode representation for the server."""
+        return unicode(self.hostname)
 
--- web/limeade/cluster/views.py
+++ web/limeade/cluster/views.py
@@ -1,3 +1,4 @@
+"""Views for limeade cluster"""
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.views.generic.list_detail import object_list, object_detail
@@ -5,84 +6,153 @@ from django.template import RequestContext
 from models import Server, Service, Region
 from forms import ServerForm, RegionForm
 
+
 @login_required
 def server_list(request):
-	return object_list(request, Server.objects.all(),
-			template_name='limeade_cluster/server_list.html')
+    """Show a list of servers.
+    
+    :param request: the request object
+    
+    :returns: a list of servers
+    """
+    return object_list(request, Server.objects.all(),
+            template_name='limeade_cluster/server_list.html')
+
 
 @login_required
 def server_add(request):
-	form = ServerForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_cluster_server_list')
-	return render_to_response("limeade_cluster/server_add.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Add a new server to the cluster.
+    
+    :param request: the request object
+    
+    :returns: an edit form template
+    """
+    form = ServerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_cluster_server_list')
+    return render_to_response("limeade_cluster/server_add.html",
+        {"form": form}, context_instance = RequestContext(request))
 
-@login_required	
+
+@login_required 
 def server_edit(request, slug):
-	server = Server.objects.get(pk=slug)
-	form = ServerForm(request.POST or None, instance=server)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_cluster_server_list')
-	return render_to_response("limeade_cluster/server_edit.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Edit a server.
+    
+    :param request: the request object
+    :param slug: the id of the server
+    
+    :returns: an edit form template
+    """
+    server = Server.objects.get(pk=slug)
+    form = ServerForm(request.POST or None, instance=server)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_cluster_server_list')
+    return render_to_response("limeade_cluster/server_edit.html",
+        {"form": form}, context_instance = RequestContext(request))
 
 
 @login_required
 def server_enable(request, slug):
-	s = get_object_or_404(Server, pk = slug)
-	s.enabled = True
-	s.save()
-	return redirect('limeade_cluster_server_list')
+    """Enable a server after a maintenance is over.
+    
+    :param request: the request object
+    :param slug: the id of the server
+    
+    :returns: redirect to server list
+    """
+    s = get_object_or_404(Server, pk = slug)
+    s.enabled = True
+    s.save()
+    return redirect('limeade_cluster_server_list')
 
 
 @login_required
 def server_disable(request, slug):
-	s = get_object_or_404(Server, pk = slug)
-	s.enabled = False
-	s.save()
-	return redirect('limeade_cluster_server_list')
+    """Put a server into maintenance.
+    
+    :param request: the request object
+    :param slug: the id of the server
+    
+    :returns: redirect to server list
+    """
+    s = get_object_or_404(Server, pk = slug)
+    s.enabled = False
+    s.save()
+    return redirect('limeade_cluster_server_list')
 
 
 @login_required
 def server_delete(request, slug):
-	s = get_object_or_404(Server, pk = slug)
-	s.delete()
-	return redirect('limeade_cluster_server_list')
-	
+    """Remove a server from the cluster.
+    
+    :param request: the request object
+    :param slug: the id of the server
+    
+    :returns: redirect to server list
+    """
+    s = get_object_or_404(Server, pk = slug)
+    s.delete()
+    return redirect('limeade_cluster_server_list')
 
 
 @login_required
 def region_list(request):
-	return object_list(request, Region.objects.all(),
-			template_name='limeade_cluster/region_list.html')
+    """Show a list of regions.
+    
+    :param request: the request object
+    
+    :returns: a list of regions
+    """
+    return object_list(request, Region.objects.all(),
+            template_name='limeade_cluster/region_list.html')
 
 
 @login_required
 def region_add(request):
-	form = RegionForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_cluster_region_list')
-	return render_to_response("limeade_cluster/region_add.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Add a new region to the cluster.
+    
+    :param request: the request object
+    
+    :returns: an edit form template
+    """
+    form = RegionForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_cluster_region_list')
+    return render_to_response("limeade_cluster/region_add.html",
+        {"form": form}, context_instance = RequestContext(request))
+
 
 @login_required
 def region_edit(request, slug):
-	region = Region.objects.get(pk=slug)
-	form = RegionForm(request.POST or None, instance=region)
-	if form.is_valid():
-		form.save()
-		return redirect('limeade_cluster_region_list')
-	return render_to_response("limeade_cluster/region_edit.html",
-		{"form": form}, context_instance = RequestContext(request))
+    """Edit a region.
+    
+    :param request: the request object
+    :param slug: the id of the region
+    
+    :returns: an edit form template
+    """
+    region = Region.objects.get(pk=slug)
+    form = RegionForm(request.POST or None, instance=region)
+    if form.is_valid():
+        form.save()
+        return redirect('limeade_cluster_region_list')
+    return render_to_response("limeade_cluster/region_edit.html",
+        {"form": form}, context_instance = RequestContext(request))
 
 
 @login_required
 def region_delete(request, slug):
-	s = get_object_or_404(Server, pk = slug)
-	s.delete()
-	return redirect('limeade_cluster_region_list')
-	
+    """Remove a region from the cluster
+    
+    :param request: the request object
+    :param slug: the id of the region
+    
+    :returns: redirect to list of regions
+    """
+    s = get_object_or_404(Server, pk = slug)
+    s.delete()
+    return redirect('limeade_cluster_region_list')
+
