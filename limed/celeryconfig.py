db4855290cfc3ee570bd0333afbbb4d95c7cd082
Thomas Merkel <tm@core.io>
2012-11-06 18:45:43 +0100

    fix new celery version BROKER_URL

--- limed/celeryconfig.py
+++ limed/celeryconfig.py
@@ -4,11 +4,8 @@ sys.path.insert(0, os.getcwd())
 
 CELERY_IMPORTS = ("cloud", "mysql")
 
-BROKER_HOST = "localhost"
-BROKER_PORT = 5672
-BROKER_USER = "limeade"
-BROKER_PASSWORD = "EimequuChuap8aa8ohyo"
-BROKER_VHOST = "limeade"
+# BROKER_URL = "transport://userid:password@hostname:port/virtual_host"
+BROKER_URL = "amqp://limeade:secret@localhost:5672/limeade"
 
 CELERY_RESULT_BACKEND = "amqp"
 
