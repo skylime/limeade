f6a674940b32da50ee0a8322c300ff47f2c18bf2
Thomas Merkel <tm@core.io>
2012-11-06 19:23:33 +0100

    fix limeade system export

--- web/limeade/system/export.py
+++ web/limeade/system/export.py
@@ -6,19 +6,18 @@ from .models import Person
 
 
 def user_export(request):
-    response = HttpResponse(mimetype='text/plain')
-    response.write(export_header())
-    user_tpl  = "$system_user = {\n"
-    group_tpl = "$system_group = {\n"
-    for p in Person.objects.all():
-        username = p.system_user_name()
-        home     = p.system_user_home()
-        uid      = p.system_user_id()
-        user_tpl  += "\t'%s' => { ensure => present, uid => %s, gid => %s, shell => '/bin/false', home => '%s', managehome => true, require => Group['%s'] }\n"
-        group_tpl += "\t'%s' => { ensure => present, gid => %s }\n"
-    user_tpl  += "}\n"
-    group_tpl += "}\n"
-    response.write(user_tpl  % (username, uid, uid, home, username))
-    response.write(group_tpl % (username, uid))
-    return response
-
+	response = HttpResponse(mimetype='text/plain')
+	response.write(export_header())
+	response.write("$system_user = {\n")
+	response.write("$system_group = {\n")
+	user_tpl  = "\t'%s' => { ensure => present, uid => %s, gid => %s, shell => '/bin/false', home => '%s', managehome => true, require => Group['%s'] }\n"
+	group_tpl = "\t'%s' => { ensure => present, gid => %s }\n"
+	for p in Person.objects.all():
+		username = p.system_user_name()
+		home     = p.system_user_home()
+		uid      = p.system_user_id()
+		response.write(user_tpl  % (username, uid, uid, home, username))
+		response.write(group_tpl % (username, uid))
+	response.write("}\n")
+	response.write("}\n")
+	return response
