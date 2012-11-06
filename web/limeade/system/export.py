c3763233b55ae48601ae3a5fdab6a63ec9344b20
Thomas Merkel <tm@core.io>
2012-11-06 21:25:35 +0000

    fix my code for the export of users and groups

--- web/limeade/system/export.py
+++ web/limeade/system/export.py
@@ -8,16 +8,18 @@ from .models import Person
 def user_export(request):
 	response = HttpResponse(mimetype='text/plain')
 	response.write(export_header())
-	response.write("$system_user = {\n")
-	response.write("$system_group = {\n")
+	users  = ""
+	groups = ""
 	user_tpl  = "\t'%s' => { ensure => present, uid => %s, gid => %s, shell => '/bin/false', home => '%s', managehome => true, require => Group['%s'] }\n"
 	group_tpl = "\t'%s' => { ensure => present, gid => %s }\n"
+
 	for p in Person.objects.all():
 		username = p.system_user_name()
 		home     = p.system_user_home()
 		uid      = p.system_user_id()
-		response.write(user_tpl  % (username, uid, uid, home, username))
-		response.write(group_tpl % (username, uid))
-	response.write("}\n")
-	response.write("}\n")
+		users  += user_tpl  % (username, uid, uid, home, username)
+		groups += group_tpl % (username, uid)
+
+	response.write("$system_user = {\n" + users + "}\n")
+	response.write("$system_group = {\n" + groups + "}")
 	return response
