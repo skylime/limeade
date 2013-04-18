eab3197d1c7ff57ccf8b7c7587030fb11a1154f8
Thomas Merkel <tm@core.io>
2013-04-18 17:06:57 +0000

    fix last entry for user-data

--- web/limeade/cloud/views/export.py
+++ web/limeade/cloud/views/export.py
@@ -6,6 +6,6 @@ def lmds_export(request, slug):
 	response = HttpResponse(mimetype='text/csv')
 	w = csv.writer(response)
 	for i in Instance.objects.filter(node__region__name=slug):
-		w.writerow([i.pk, i.hostname, i.mac_addr, i.ip])
+		w.writerow([i.pk, i.hostname, i.mac_addr, i.ip, ""])
 	
 	return response
